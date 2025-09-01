b9633050765ee6f23d6abcea02fefa2370a83cac
Tilottama Shinde <162155750+TilottamaShinde@users.noreply.github.com>
2025-09-01 13:55:59 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from utils import init_db
+
+def main():
+    init_db()
+    while True:
+        print("\n===HRMS Lite===\n")
+        print("1. Employee Management")
+        print("2. Attendance")
+        print("3. Leave Management")
+        print("4. Payroll")
+        print("5. Reports ")
+        print("0. Exit")
+
+        choice = input("Enter your choice : ")
+        if choice == "0":
+            print("Exiting.... Bye...")
+            break
+        else:
+            print("Feature not yet implemented.")
+
+if __name__ == "__main__":
+    main()

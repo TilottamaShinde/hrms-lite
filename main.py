ccc17d463b08c6ddccec06d34a712f1dca8f1c90
Tilottama Shinde <162155750+TilottamaShinde@users.noreply.github.com>
2025-09-21 22:45:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ from utils import init_db
 def main():
     init_db()
     while True:
-        
-        print("\n===HRMS Lite===\n")
+        print("\n=== HRMS Lite===\n")
         print("1. Add Employee")
-        print("2. View Employee")
-        print("3. Delete Employee")
-        print("4. Exit")
-       """ print("1. Employee Management")
-        print("2. Attendance")
-        print("3. Leave Management")
-        print("4. Payroll")
-        print("5. Reports ")
-        print("0. Exit")"""
+        print("2. View Employees")
+        print("3. Delete Employee ")
+        print("4. Mark Attendance")
+        print("5. View Attendance  ")
+        print("6. Apply Leave")
+        print("7. View Leaves")
+        print("8. Approve/Reject Leave ")
+        print("9. Generate Payslip")
+        print("10. View Payroll")
+        print("0. Exit")
 
         choice = input("Enter your choice : ")
         if choice == "0":
